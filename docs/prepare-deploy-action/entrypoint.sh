79c2bdd7845280c2d912e9ab42f88d262164a0e9
Gregor <gregor@martynus.net>
2019-04-15 09:27:13 -0700

    WIP

--- docs/prepare-deploy-action/entrypoint.sh
+++ docs/prepare-deploy-action/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-npm ci --only=production
+npm install @octokit/routes # installs version defined in /package.json
 npm --prefix ./docs install ./docs
 npm run build --prefix ./docs -- --prefix-paths
